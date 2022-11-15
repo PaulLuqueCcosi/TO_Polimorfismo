86f94de81034d68051107b698628e3697f70eb60
PaulLuqueCcosi <pluquec@unsa.edu.pe>
2022-11-15 16:49:33 -0500

    funciona herencia con clase

--- c++/Clase/Main.cc
+++ c++/Clase/Main.cc
@@ -1,12 +1,23 @@
 #include "Casa.h"
 #include "Inmueble.h"
+#include "Departamento.h"
 
 int main() {
-    Inmueble * a = new Casa();
+    Inmueble * a = new Departameto();
+    Inmueble * b = new Casa();
+    Inmueble * c = new Inmueble();
     //Casa * a = new Casa();
     a->vender();
     a->calcularArea();
     a->calcularPrecio();
+
+    b->vender();
+    b->calcularArea();
+    b->calcularPrecio();
+
+    c->vender();
+    c->calcularArea();
+    c->calcularPrecio();
 
     return 0;
 }
